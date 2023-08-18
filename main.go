cd6430e7608071977eac87c95119960a06120b37
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-18 21:46:40 +0330

    Creating cfg and logger in main

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"book-manager-service/DataAccess"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	var cfg DataAccess.Config
+	err := cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	logger := logrus.New()
+	logger.SetLevel(logrus.DebugLevel)
+	logger.SetReportCaller(true)
+	logger.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+}
