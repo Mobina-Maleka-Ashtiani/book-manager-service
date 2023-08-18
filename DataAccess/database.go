a8a422c1ffdd825f57f79f73ef1680d7d3889b71
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-18 21:54:50 +0330

    Creating User struct and gender type in DataAccess\database.go

--- DataAccess/database.go
+++ DataAccess/database.go
@@ -11,6 +11,28 @@ type GormDB struct {
 	db  *gorm.DB
 }
 
+type User struct {
+	gorm.Model
+	FirstName   string `gorm:"varchar(50)" json:"first_name"`
+	LastName    string `gorm:"varchar(50)" json:"last_name"`
+	Gender      Gender `gorm:"varchar(25)" json:"gender"`
+	PhoneNumber string `gorm:"varchar(30),unique" json:"phone_number"`
+	Username    string `gorm:"varchar(50),unique" json:"username"`
+	Email       string `gorm:"varchar(50),unique" json:"email"`
+	Password    string `gorm:"varchar(64)" json:"password"`
+}
+
+type Gender string
+
+const (
+	Female      Gender = "female"
+	Male        Gender = "male"
+	NonBinary   Gender = "non-binary"
+	Transgender Gender = "transgender"
+	Intersex    Gender = "intersex"
+	Other       Gender = "i prefer not to say"
+)
+
 func NewGormDB(cfg Config) (*GormDB, error) {
 	c := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
 		cfg.Database.Host,
@@ -20,7 +42,6 @@ func NewGormDB(cfg Config) (*GormDB, error) {
 		cfg.Database.Password,
 	)
 
-	// Create a new connection
 	db, err := gorm.Open(postgres.Open(c), &gorm.Config{})
 	if err != nil {
 		return nil, err
