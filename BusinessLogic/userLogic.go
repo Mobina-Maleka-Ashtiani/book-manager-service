f9ac50f3951362b9dd8cf9250a2dc75e9798f00c
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-18 22:10:35 +0330

    Creating AddUser func in BusinessLogic\userLogic.go

--- BusinessLogic/userLogic.go
+++ BusinessLogic/userLogic.go
@@ -1,1 +1,28 @@
 package BusinessLogic
+
+import (
+	"book-manager-service/DataAccess"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func AddUser(gdb *DataAccess.GormDB, user DataAccess.User) error {
+	if gdb.UsernameExistence(user.Username) {
+		return errors.New("this username already taken")
+	}
+
+	if gdb.EmailExistence(user.Email) {
+		return errors.New("this email already taken")
+	}
+
+	if gdb.PhoneNumberExistence(user.PhoneNumber) {
+		return errors.New("this phone number already taken")
+	}
+
+	if pw, err := bcrypt.GenerateFromPassword([]byte(user.Password), 0); err != nil {
+		//log.Println(err, " password not hash")
+		return err
+	} else {
+		user.Password = string(pw)
+	}
+}
