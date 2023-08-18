ff9ecc2f0d079b83aa6511df68b6acea75707169
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-19 00:00:48 +0330

    Creating generateRandomSecretKey func and secretKey variable in BusinessLogic\jwtManager.go

--- BusinessLogic/jwtManager.go
+++ BusinessLogic/jwtManager.go
@@ -1,1 +1,16 @@
 package BusinessLogic
+
+import (
+	"crypto/rand"
+)
+
+var secretKey, _ = generateRandomSecretKey()
+
+func generateRandomSecretKey() ([]byte, error) {
+	jwtKey := make([]byte, 32)
+	if _, err := rand.Read(jwtKey); err != nil {
+		return nil, err
+	}
+
+	return jwtKey, nil
+}
