460fbb4db2a15b5eac84157040084531beeaf4f2
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-18 23:15:56 +0330

    Creating firstNameValidation and lastNameValidation funcs in Presentation\inputValidation.go

--- Presentation/inputValidation.go
+++ Presentation/inputValidation.go
@@ -1,1 +1,29 @@
 package Presentation
+
+import "strings"
+
+func firstNameValidation(firstName string) bool {
+	str := "abcdefghijklmnopqrtsuvwxyzABCDEFGEHIJKLMNOPQRSTUVWXYZ"
+	if len(firstName) < 1 {
+		return false
+	}
+	for i := 0; i < len(firstName); i++ {
+		if !strings.Contains(str, string(firstName[i])) {
+			return false
+		}
+	}
+	return true
+}
+
+func lastNameValidation(lastName string) bool {
+	str := "abcdefghijklmnopqrtsuvwxyzABCDEFGEHIJKLMNOPQRSTUVWXYZ"
+	if len(lastName) < 1 {
+		return false
+	}
+	for i := 0; i < len(lastName); i++ {
+		if !strings.Contains(str, string(lastName[i])) {
+			return false
+		}
+	}
+	return true
+}
