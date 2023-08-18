f4ec56e1ef2bb29ea174cb7c00e92bdd330df269
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-18 22:05:57 +0330

    Creating usernameExistence func in DataAccess\userDataMethods.go

--- DataAccess/userDataMethods.go
+++ DataAccess/userDataMethods.go
@@ -1,1 +1,8 @@
 package DataAccess
+
+func (gdb *GormDB) UsernameExistence(username string) bool {
+	var count int64
+	gdb.db.Model(&User{}).Where(&User{Username: username}).Count(&count)
+
+	return count > 0
+}
