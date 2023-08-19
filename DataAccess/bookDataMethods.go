17a4cdaaee6748ad9e3e0299e3d3ba496a4a7fd9
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-20 00:49:21 +0330

    Creating GetAllBooks func in DataAccess\bookDataMethods.go

--- DataAccess/bookDataMethods.go
+++ DataAccess/bookDataMethods.go
@@ -1,1 +1,9 @@
 package DataAccess
+
+func (gdb *GormDB) GetAllBooks() ([]Book, error) {
+	var books []Book
+	if err := gdb.db.Preload("Authors").Find(&books).Error; err != nil {
+		return nil, err
+	}
+	return books, nil
+}
