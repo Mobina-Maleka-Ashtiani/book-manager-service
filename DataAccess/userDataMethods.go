27f08d09aa44d9095ded2d3c1d47fe87f4002635
Mobina-Maleka-Ashtiani <mobina.ashtiani@gmail.com>
2023-08-19 22:07:39 +0330

    Creating AddBookToUser funcs in DataAccess\userDataMethods.go

--- DataAccess/userDataMethods.go
+++ DataAccess/userDataMethods.go
@@ -2,21 +2,21 @@ package DataAccess
 
 func (gdb *GormDB) UsernameExistence(username string) bool {
 	var count int64
-	gdb.db.Model(&User{}).Where(&User{Username: username}).Count(&count)
+	gdb.db.Model(&User{}).Where("username = ?", username).Count(&count)
 
 	return count > 0
 }
 
 func (gdb *GormDB) EmailExistence(email string) bool {
 	var count int64
-	gdb.db.Model(&User{}).Where(&User{Email: email}).Count(&count)
+	gdb.db.Model(&User{}).Where("email = ?", email).Count(&count)
 
 	return count > 0
 }
 
 func (gdb *GormDB) PhoneNumberExistence(phoneNumber string) bool {
 	var count int64
-	gdb.db.Model(&User{}).Where(&User{PhoneNumber: phoneNumber}).Count(&count)
+	gdb.db.Model(&User{}).Where("phone_number = ?", phoneNumber).Count(&count)
 
 	return count > 0
 }
@@ -27,10 +27,18 @@ func (gdb *GormDB) AddUserToDatabase(user *User) error {
 
 func (gdb *GormDB) FindUserByUsername(username string) (*User, error) {
 	var user User
-	err := gdb.db.Where(&User{Username: username}).First(&user).Error
+	err := gdb.db.Where("username = ?", username).Preload("Books").First(&user).Error
 	if err != nil {
 		return nil, err
 	}
 
 	return &user, nil
+}
+
+func (gdb *GormDB) AddBookToUser(user User, book Book) error {
+	user.Books = append(user.Books, &book)
+	if err := gdb.db.Save(&user).Error; err != nil {
+		return err
+	}
+	return nil
 }
